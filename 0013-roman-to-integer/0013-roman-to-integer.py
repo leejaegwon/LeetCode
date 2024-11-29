41206cafb1ba4493528eb63854ce55a2db1f65ee
jaegwon <119105436+leejaegwon@users.noreply.github.com>
2024-11-29 14:07:54 +0900

    Time: 4 ms (66.01%), Space: 17.4 MB (18%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,24 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romanMap = { "":0,
+            "I":1, "V":5, "X":10,
+            "L":50, "C":100, "D": 500, 
+            "M":1000
+        }
+        sum = 0
+        lastword = ""
+        for string in s:
+            if lastword == "I":
+                if string == "V" or string == "X":
+                    sum -= 2 * romanMap[lastword]
+            elif lastword == "X":
+                if string == "L" or string == "C":
+                    sum -= 2 * romanMap[lastword]
+            elif lastword == "C":
+                if string == "D" or string == "M":
+                    sum -= 2 * romanMap[lastword]
+            sum += romanMap[string]
+            lastword = string
+        
+        return sum
+        
