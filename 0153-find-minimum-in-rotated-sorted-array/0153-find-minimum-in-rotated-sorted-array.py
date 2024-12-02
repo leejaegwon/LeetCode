a4a57b9d4a635c0083a94ff162ccbb3336ad708b
jaegwon <119105436+leejaegwon@users.noreply.github.com>
2024-12-02 22:46:28 +0900

    Time: 0 ms (100%), Space: 17.5 MB (5.08%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,14 +1,13 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        # 기본적으로 ascending
-        # 증가하다가 감소한 index를 만날텐데 이것이 진짜 작은거냐?
-        n = len(nums)
-        dn = nums[0]
-        for i in range(1,len(nums)):
-            if dn < nums[i]:
-                dn = nums[i]
+        # left mid right
+        l,r = 0, len(nums) - 1
+        while l<r:
+            mid = (l + r) // 2
+            if nums[mid] > nums[r]:
+                l = mid + 1
             else:
-                return nums[i]
-        return nums[0]
+                r = mid
+        return nums[l]
 
         
